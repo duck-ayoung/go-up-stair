e0171b5d81df70ee1429cd07dbe3a93ef9cbf0c2
duck-ayoung <duck.ayoung@gmail.com>
2022-02-12 23:03:38 +0900

    create project

---
+++ src/main/java/com/duck/ayoung/goupstair/GoUpStairApplication.java
@@ -0,0 +1,13 @@
+package com.duck.ayoung.goupstair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoUpStairApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoUpStairApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duck/ayoung/goupstair/GoUpStairApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duck.ayoung.goupstair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoUpStairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
