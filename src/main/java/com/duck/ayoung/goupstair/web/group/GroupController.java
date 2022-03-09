a7a9d5fe2c9eb258c3067c532a864d2316004b9d
duck-ayoung <duck.ayoung@gmail.com>
2022-03-09 12:20:23 +0900

    add group invite control

--- src/main/java/com/duck/ayoung/goupstair/web/group/GroupController.java
+++ src/main/java/com/duck/ayoung/goupstair/web/group/GroupController.java
@@ -1,8 +1,10 @@
 package com.duck.ayoung.goupstair.web.group;
 
 import com.duck.ayoung.goupstair.domain.Member;
+import com.duck.ayoung.goupstair.domain.SearchTypes;
 import com.duck.ayoung.goupstair.repository.RankInfo;
 import com.duck.ayoung.goupstair.service.GroupService;
+import com.duck.ayoung.goupstair.service.MemberService;
 import com.duck.ayoung.goupstair.web.argumentresolver.Login;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,12 +20,35 @@ import java.util.List;
 @RequestMapping("/groups")
 public class GroupController {
 
+    private final MemberService memberService;
     private final GroupService groupService;
 
+    @ModelAttribute("searchTypes")
+    public SearchTypes[] searchTypes() {
+        return SearchTypes.values();
+    }
+
     @GetMapping("/{groupId}")
-    public String mainGroupView(@PathVariable Long groupId, Model model) {
+    public String mainGroupView(@PathVariable Long groupId, Model model, @ModelAttribute String searchValue) {
         List<RankInfo> rankInfo = groupService.findRankInfo(groupId);
         model.addAttribute("rankInfo", rankInfo);
+        return "groups/main";
+    }
+
+    @PostMapping("/{groupId}")
+    public String search(@PathVariable Long groupId, Model model, @ModelAttribute String searchValue) {
+        List<RankInfo> rankInfo = groupService.findRankInfo(groupId);
+        model.addAttribute("rankInfo", rankInfo);
+        List<Member> members = memberService.findByLoginIdContaining(searchValue);
+        model.addAttribute("members", members);
+        return "groups/main";
+    }
+
+    @GetMapping("/{groupId}/invite/{memberId}")
+    public String invite(@PathVariable Long groupId, @PathVariable Long memberId, Model model, @ModelAttribute String searchValue) {
+        List<RankInfo> rankInfo = groupService.findRankInfo(groupId);
+        model.addAttribute("rankInfo", rankInfo);
+        groupService.joinGroup(groupId, memberId);
         return "groups/main";
     }
 
